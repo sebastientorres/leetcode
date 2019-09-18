14da2d7b5e76eaa65494f5922b0b23cf2caf6b78
Sebastien Torres <sebastien.torres@pgs.com>
2019-09-18 15:55:26 +0100

    JUnit 5 Parameterised tests. TwoSum algo impl.

--- src/main/java/com/sebastien/TwoSum.java
+++ src/main/java/com/sebastien/TwoSum.java
@@ -5,6 +5,20 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] results = new int[2];
 
+
+        for (int i = 0 ; i < nums.length; i++) {
+            for (int k = 1; k < nums.length; k++) {
+                int a = nums[i];
+                int b = nums[k];
+
+                if (a + b == target) {
+                    results[0] = i;
+                    results[1] = k;
+                    break;
+                }
+            }
+        }
+
         return results;
     }
 }
--- src/test/java/com/sebastien/TwoSumTest.java
+++ src/test/java/com/sebastien/TwoSumTest.java
@@ -1,14 +1,16 @@
 package com.sebastien;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.Is.is;
 
-class TwoSumTest {
+public class TwoSumTest {
 
-    @Test
-    void twoSum() {
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void twoSum(int[] input, int target, int[] expectedIndecies) {
 
         TwoSum twoSum = new TwoSum();
         int[] results = twoSum.twoSum(input, target);
@@ -16,12 +18,15 @@ class TwoSumTest {
         assertThat("Length", results.length, is(2));
 
         for (int i = 0; i < 2; i++) {
-            assertThat("Index", results[i], is(expectedIndeices[i]));
+            assertThat("Index", results[i], is(expectedIndecies[i]));
         }
-
     }
 
-    int[] input = new int[] {2, 7, 11, 15};
-    int target = 9;
-    int[] expectedIndeices = new int[] {0, 1};
+    static Collection parameters() {
+        return Arrays.asList(new Object[][] {
+                // input, target, expectedIndices
+                {new int[] {2, 7, 11, 15}, 9, new int[] {0, 1}}
+        });
+    }
+
 }

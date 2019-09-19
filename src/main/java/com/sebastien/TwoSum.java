faf725978f3d3502f6e48e905de510e163e0dba2
Sebastien Torres <sebastien.torres@pgs.com>
2019-09-19 09:06:15 +0100

    Simplify the solution a little; remove u-nneeded variable, return early.

--- src/main/java/com/sebastien/TwoSum.java
+++ src/main/java/com/sebastien/TwoSum.java
@@ -3,24 +3,18 @@ package com.sebastien;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        int[] results = new int[2];
 
-        boolean found = false;
-
-        for (int i = 0; i < nums.length && !found; i++) {
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j< nums.length; j++) {
                 int a = nums[i];
                 int b = nums[j];
 
                 if(a+b == target) {
-                    results[0] = i;
-                    results[1] = j;
-                    found = true;
-                    break;
+                    return new int[] {i, j};
                 }
             }
         }
 
-        return results;
+        return new int[] {-1, -1};
     }
 }
